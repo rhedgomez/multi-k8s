a3242deec10edcb1ad29d596aa1c365cacb0f72b
rhedgomez <rhedil.gomez@gmail.com>
2019-04-30 10:12:41 -0600

    revert back to original

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ docker push rhedgomez/multi-worker:$SHA
  
 kubectl apply -f k8s #i think dire nimo himo-on ang helm install, but how?
 
-#set image imperative command
+#set image imperative command; to  always tell kubernetes to use the updated image
 kubectl set image deployments/server-deployment server=rhedgomez/multi-server:$SHA
-kubectl set image deployments/client-deployment client=rhedgomez/multi-client:testing-image
+kubectl set image deployments/client-deployment client=rhedgomez/multi-client:$SHA
 #this is how kubernetes is going to use the latest image: example: kubectl set image deployments/client-deployment client=rhedgomez/multi-client:testing-image
 kubectl set image deployments/worker-deployment worker=rhedgomez/multi-worker:$SHA
 #server=rhedgomez/multi-server; server= is like the server container inside this deployment needs to use the image that is explicitly specified
