a75d3ec59ea8ebd89452876d09b04d680cfbef1e
rhedgomez <rhedil.gomez@gmail.com>
2019-05-02 09:27:18 -0600

    update welcome page

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version HELM, IT WORKS! YES!</h1>
+            <h1 className="App-title">Fib Calculator version HELM, IT WORKS!!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Pages</Link>
           </header>
